1f58ed383ccf02e3d9e532c02a8f44a7b5f65670
kVIN3143 <kevinrahul3143@gmail.com>
2025-06-25 20:57:02 +0530

    Dog class added

---
+++ Dog.java
@@ -0,0 +1,8 @@
+public class Dog extends Animal {
+    public void move() {
+        System.out.println("dog can walk in 2 Legs");
+    }
+    public void bark() {
+        System.out.println("Woof!!! Woof!!!");
+    }
+}

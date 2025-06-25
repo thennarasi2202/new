4272196b92be29ea1903a0d76c24e81831779c50
thennarasi0203 <thennarasi0203@gmail.com>
2025-06-25 21:06:32 +0530

    changed

--- Cat.java
+++ Cat.java
@@ -2,4 +2,8 @@ class Cat extends Animal {
     void move() {
         System.out.println("Cat moves with four legs");
     }
+
+    void purrs() {
+        System.out.println("Cat purrs when it is calm");
+    }
 }

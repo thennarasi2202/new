af3957e034156abfc0eecb8a95f3523e78132610
Orville-pixel <jobz5654@gmail.com>
2025-06-25 22:41:58 +0530

    commit changed

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Dog shihTzu = new Dog();
+        shihTzu.move();
+        shihTzu.bark();
+
+        Cat persian = new Cat();
+        persian.move();
+        persian.purrs();
+    }
+}
